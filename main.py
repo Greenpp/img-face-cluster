526f30c39eac520eb816ba81ea319686a4ec7cd2
Jakub Ciszek <jakubciszek64@gmail.com>
2020-11-19 14:37:43 +0100

    Test with config

--- main.py
+++ main.py
@@ -1,80 +1,27 @@
-import glob
-import pickle as pkl
-
-import numpy as np
-import torch
-from facenet_pytorch import MTCNN, InceptionResnetV1
-from PIL import Image
-from torch.nn.functional import l1_loss
+from matplotlib import pyplot as plt
 
 import config
-
-from .detection import get_faces_from_img
-from .image import load_img
-from .recognition import get_faces_embedding
-
-
-def config_device() -> str:
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
-    return device
-
-
-def add_person(
-    d: dict, face: Image.Image, embedding: torch.Tensor, f_name: str
-) -> None:
-    new_idx = len(d) + 1
-    d[f'person_{new_idx}'] = {
-        'face': np.array(face),
-        'embedding': embedding,
-        'imgs': set([f_name]),
-    }
-
-
-def save_people(people: dict) -> None:
-    save_path = f'{config.BASE_DIR}/face_recognition_cache.pkl'
-    with open(save_path, 'wb') as f:
-        pkl.dump(people, f, pkl.HIGHEST_PROTOCOL)
-
-
-# TODO describe everything
+from scanner import Scanner
 
 if __name__ == "__main__":
-    device = config_device()
-    mtcnn = MTCNN(keep_all=True, device=device)
-    resnet = InceptionResnetV1(pretrained='vggface2').eval()
+    sc = Scanner()
 
-    people = {}
-    other = set()
-    for extension in config.IMG_EXT:
-        search_pattern = f'{config.BASE_DIR}/**/*.{extension}'
-        base_len = len(config.BASE_DIR) + 1
-        for f_name in glob.iglob(search_pattern, recursive=True):
-            f_name = f_name[base_len:]
-            img = load_img(f_name)
+    sc.scan(
+        search_dir=config.BASE_DIR,
+        extensions=config.IMG_EXT,
+        detection_threshold=config.DETECTION_THRESHOLD,
+        recognition_threshold=config.RECOGNITION_THRESHOLD,
+        img_resize_width=config.MAX_IMG_WIDTH,
+        img_resize_height=config.MAX_IMG_HEIGHT,
+        verbose=True,
+    )
 
-            faces = get_faces_from_img(img, mtcnn)
-            if not faces:
-                other.add(f_name)
-            else:
-                embeddings = get_faces_embedding(faces, resnet)
-
-                for face, embedding in zip(faces, embeddings.unbind()):
-                    new_idx = len(people) + 1
-                    if not people:
-                        add_person(people, face, embedding, f_name)
-                    else:
-                        found = False
-                        for person in people.values():
-                            distance = l1_loss(embedding, person['embedding']).item()
-                            if distance < config.RECOGNITION_THRESHOLD:
-                                found = True
-                                person['imgs'].add(f_name)
-                                break
-
-                        if not found:
-                            add_person(people, face, embedding, f_name)
-
-    people['none'] = {'imgs': other}
-
-    save_people(people)
+    for person, person_dict in sc.people.items():
+        print(f'Person: {person}')
+        for img in person_dict['paths']:
+            print(img)
+        if person != 'none':
+            face = person_dict['face']
+            plt.imshow(face)
+            plt.show()
+        print('')
