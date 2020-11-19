a5ab4959a33a9fc9753a7655253f1fa0a4892252
Jakub Ciszek <jakubciszek64@gmail.com>
2020-11-19 18:59:40 +0100

    Minimal setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+from setuptools import setup, find_packages
+
+setup(
+    name='ImgFaceSorter',
+    version='0.0.1',
+    url='https://github.com/Greenpp/face-recognition-img-sort',
+    author='Jakub Ciszek',
+    packages=find_packages(),
+)
