e6beb786670402aef9da2b13cab39ffc3f886476
Jakub Ciszek <jakubciszek64@gmail.com>
2021-08-11 16:01:25 +0200

    Packages updated

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='FaceCluster',
-    version='0.0.1',
-    url='https://github.com/Greenpp/face-recognition-img-sort',
-    author='Jakub Ciszek',
-    packages=find_packages(),
-)
