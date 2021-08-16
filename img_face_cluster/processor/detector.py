65e1a6cef6d51520f9c238206342a047d52ef817
Jakub Ciszek <jakubciszek64@gmail.com>
2021-08-16 22:51:23 +0200

    Bounding boxes returned

--- img_face_cluster/processor/detector.py
+++ img_face_cluster/processor/detector.py
@@ -1,21 +1,19 @@
+import numpy as np
 from facenet_pytorch import MTCNN
 from PIL import Image
 from torch import Tensor
 
 
 class Detector:
-    def __init__(self, face_threshold: float = 0.0, gpu: bool = True) -> None:
+    def __init__(self, gpu: bool = True) -> None:
         device = 'cuda' if gpu else 'cpu'
         self.model = MTCNN(keep_all=True, device=device)
 
-        self.face_threshold = face_threshold
+    def extract_faces(
+        self,
+        img: Image.Image,
+    ) -> tuple[Tensor, np.ndarray, np.ndarray]:
+        boxes, probs = self.model.detect(img)
+        faces = self.model.extract(img, boxes, None)
 
-    def extract_faces(self, img: Image.Image) -> list[Tensor]:
-        faces, probs = self.model(img, return_prob=True)
-
-        filtered_faces = []
-        for face, prob in zip(faces, probs):
-            if prob > self.face_threshold:
-                filtered_faces.append(face)
-
-        return filtered_faces
+        return faces, probs, boxes
